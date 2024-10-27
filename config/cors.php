ce5717762b2e46f8e01106b6b40a1ac644cca837
Crhistian Ramirez Lopez <crhis.rl@gmail.com>
2024-10-27 14:06:44 -0500

    Delete missing code

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
     
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://wellezy-front.vercel.app/','http://localhost:5173','https://web-production-8f66.up.railway.app','http://127.0.0.1:5173'],
+    'allowed_origins' => ['https://wellezy-front.vercel.app','http://localhost:5173','https://web-production-8f66.up.railway.app','http://127.0.0.1:5173'],
 
     'allowed_origins_patterns' => [],
 
